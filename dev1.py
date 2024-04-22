66214337ad02067e059bbab008fc5b005f1762c1
code-puccino <yingyingp22@gmail.com>
2024-04-22 18:39:56 +0200

    change comment

--- dev1.py
+++ dev1.py
@@ -1,7 +1,7 @@
 
 
 
-# Press the green button in the gutter to run the script.
+# change a comment that is deleted
 if __name__ == '__main__':
     print('some code..line1')
     print('some code...sample code....')
